b8fca433df0a69250b3a20e1c7ef0b28cfadf2c9
Wisanu <wisanu2308@gmail.com>
2022-06-10 15:06:39 +0700

    update

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-const request = require('request');
-const dotenv = require('dotenv');
-dotenv.config();
+const express = require('express')
+const app = express()
+const port = 3000
 
-const url_line_notification = "https://notify-api.line.me/api/notify";
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-request({
-    method: 'POST',
-    uri: url_line_notification,
-    header: {
-        'Content-Type': 'multipart/form-data',
-    },
-    auth: {
-        bearer: process.env.TOKEN,
-    },
-    form: {
-        message: 'Test Message!'
-    },
-}, (err, httpResponse, body) => {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log(body)
-    }
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
